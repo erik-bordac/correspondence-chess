d36d2eb0599e3cc317ac340bf635f79a902cd9c2
erikb <erik.bordac@gmail.com>
2023-03-29 19:11:10 +0200

    remove commented out code

--- Chess_SchoolProject/ChessFigures/Rook.cs
+++ Chess_SchoolProject/ChessFigures/Rook.cs
@@ -19,63 +19,6 @@ namespace Chess_SchoolProject.ChessFigures
 
 			ImgPath = (Color == "W") ? "./Resources/Wrook.png" : "./Resources/Brook.png";
 		}
-
-		//public bool IsValidMove(Square source, Square target, ChessGame game)
-		//{
-		//	if (target.Content != null && source.Content.Color == target.Content.Color) return false;
-
-		//	bool row1 = true;
-		//	bool row2 = true;
-		//	bool file1 = true;
-		//	bool file2 = true;
-		//	for (int i = 1; i < 8; i++)
-		//	{
-		//		if (row1)
-		//		{
-		//			int checkedRow = source.Row + i;
-
-		//			if (checkedRow > 7) row1 = false;
-
-		//			if (row1 && game.gameArr[checkedRow][source.File].Content != null) row1 = false;
-
-		//			if (checkedRow == target.Row && source.File == target.File) { return true; }
-		//		}
-		//		if (row2)
-		//		{
-		//			int checkedRow = source.Row - i;
-
-		//			if (checkedRow < 0 ) row2 = false;
-
-		//			if (row2 && game.gameArr[checkedRow][source.File].Content != null) row2 = false;
-
-		//			if (checkedRow == target.Row && source.File == target.File) { return true; }
-		//		}
-
-		//		if (file1)
-		//		{
-		//			int checkedFile = source.File + i;
-
-		//			if (checkedFile > 7 ) file1 = false;
-
-		//			if (file1 && game.gameArr[source.Row][checkedFile].Content != null) file1 = false;
-
-		//			if (source.Row == target.Row && checkedFile == target.File) { return true; }
-		//		}
-
-		//		if (file2)
-		//		{
-		//			int checkedFile = source.File - i;
-
-		//			if (checkedFile < 0 ) file2 = false;
-
-		//			if (file2 && game.gameArr[source.Row][checkedFile].Content != null) file2 = false;
-
-		//			if (source.Row == target.Row && checkedFile == target.File) { return true; }
-		//		}
-		//	}
-		//	return false;
-		//}
-
 		public bool IsValidMove(Square source, Square target, ChessGame game)
 		{
 			// Check if the source and target squares are the same or if the target square contains a piece of the same color
